66857a5288a494662376b90f376b34cf814797a4
Hakim Hauston <hakim.hauston@gmail.com>
2017-06-12 00:39:35 +0800

    added TODO controllers

---
+++ app/controllers/WidgetsController.scala
@@ -0,0 +1,16 @@
+package controllers
+
+import play.api.mvc._
+
+class WidgetsController extends Controller {
+  
+  def index = TODO
+  
+  def create = TODO
+  def read(id: String) = Action {
+    Ok("Hello world!");
+  }
+  def update(id: String) = TODO
+  def delete(id: String) = TODO
+  
+}

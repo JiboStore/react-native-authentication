71e51df9b46dfdb4dce5a348ba5e698f879d797b
Hakim Hauston <hakim.hauston@igg.com>
2017-07-28 17:32:34 +0800

    [seeder] request: 104 - 200

--- getmanga.sh
+++ getmanga.sh
@@ -1,4 +1,4 @@
-curl 'http://localhost:9000/manga/seeder/getfromto?startingIndex=103&endingIndex=200' > 103_200.txt
+curl 'http://localhost:9000/manga/seeder/getfromto?startingIndex=104&endingIndex=200' > 104_200.txt
 git add --all
-git commit -am "[seeder] manga: 103 - 200"
-git push origin HEAD:103
+git commit -am "[seeder] manga: 104 - 200"
+git push origin HEAD:104

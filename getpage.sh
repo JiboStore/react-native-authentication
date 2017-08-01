a71457d1f63f9e7849aad5e64d171c5b27345da2
Hakim Hauston <hakim.hauston@gmail.com>
2017-08-02 06:35:49 +0800

    [mangafun] download page images 0 - 38 downloaded, next start from 39 (337 byooshi)

--- getpage.sh
+++ getpage.sh
@@ -1,4 +1,5 @@
-curl 'http://localhost:9000/manga/seeder/imagefromto?startingIndex=0&endingIndex=100' > img_0_100.txt
-git add --all
-git commit -am "[seeder] img: 0 - 100"
-git push origin HEAD:IMG_0
+curl 'http://localhost:9000/manga/seeder/imagefromto?startingIndex=39&endingIndex=200' > img_39_200.txt
+# 39 - 337 byooshi
+#git add --all
+#git commit -am "[seeder] img: 0 - 100"
+#git push origin HEAD:IMG_0

f7fd2eeaeb3e1d4fefb467105e75b11cdee781f7
Hakim Hauston <hakim.hauston@gmail.com>
2017-08-04 06:48:59 +0800

    [mangafun] manga: 55 - 79
    [error] application - downloaded pages for manga: 55 : 666 Satan
    [error] application - downloaded pages for manga: 56 : 7 Jikan-me Rhapsody
    [error] application - downloaded pages for manga: 57 : 7-nin no Shakespeare
    [error] application - downloaded pages for manga: 58 : 7 Seeds
    [error] application - manga json not found: 70
    [error] application - downloaded pages for manga: 59 : 70
    [error] application - downloaded pages for manga: 60 : 703X
    [error] application - downloaded pages for manga: 61 : 7th Dragon
    [error] application - downloaded pages for manga: 62 : 7th Period Is A Secret
    [error] application - downloaded pages for manga: 63 : 8.1 - Yamada Yuusuke Gekijou
    [error] application - downloaded pages for manga: 64 : 81 Diver
    [error] application - downloaded pages for manga: 65 : 87 Clockers
    [error] application - manga json not found: A+B
    [error] application - downloaded pages for manga: 66 : A+B
    [error] application - downloaded pages for manga: 67 : A Bias Girl
    [error] application - downloaded pages for manga: 68 : A Book of Dreams
    [error] application - manga json not found: A-bout!
    [error] application - downloaded pages for manga: 69 : A-bout!
    [error] application - downloaded pages for manga: 70 : A Channel
    [error] application - downloaded pages for manga: 71 : A Fairytale for the Demon Lord
    [error] application - downloaded pages for manga: 72 : A Falsified Romance
    [error] application - downloaded pages for manga: 73 : A Lollipop or a Bullet
    [error] application - downloaded pages for manga: 74 : A Love Guard
    [error] application - downloaded pages for manga: 75 : A Man for Megan
    [error] application - downloaded pages for manga: 76 : A Method to Make the World Gentle
    [error] application - downloaded pages for manga: 77 : A Midsummer Night's Dream
    [error] application - downloaded pages for manga: 78 : A Rose-Colored Tomorrow
    [error] application - downloaded pages for manga: 79 : A Simple Thinking About Blood Types

--- getpage.sh
+++ getpage.sh
@@ -1,4 +1,4 @@
-curl 'http://localhost:9000/manga/seeder/imagefromto?startingIndex=39&endingIndex=200' > img_39_200.txt
+curl 'http://localhost:9000/manga/seeder/imagefromto?startingIndex=80&endingIndex=200' > img_39_200.txt
 # 39 - 337 byooshi
 #git add --all
 #git commit -am "[seeder] img: 0 - 100"

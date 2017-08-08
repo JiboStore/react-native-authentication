37986f37d115b43b9ff6136ade6c3da50110a53f
ios.resources.builder <ios.resources.builder@igg.com>
2017-08-08 18:35:24 +0800

    [seeder] manga: 501 - 1001

--- getmanga.sh
+++ getmanga.sh
@@ -1,4 +1,4 @@
-curl 'http://localhost:9000/manga/seeder/getfromto?startingIndex=500&endingIndex=1001' > 500_1001.txt
+curl 'http://localhost:9000/manga/seeder/getfromto?startingIndex=501&endingIndex=1001' > 501_1001.txt
 git add --all
-git commit -am "[seeder] manga: 500 - 1001"
-git push origin HEAD:500
+git commit -am "[seeder] manga: 501 - 1001"
+git push origin HEAD:501

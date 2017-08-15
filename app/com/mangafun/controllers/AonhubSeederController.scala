acf0b9f0217d62887606bcc7fc78fe230f7261aa
Hakim Hauston <hakim.hauston@gmail.com>
2017-08-15 12:06:42 +0800

    [mangafun] aonhub: todo: download the pages
    How to download all pages in a manga:
    curl http://localhost:3005/aonhub/seeder/image?mangaId=2940

--- app/com/mangafun/controllers/AonhubSeederController.scala
+++ app/com/mangafun/controllers/AonhubSeederController.scala
@@ -48,8 +48,27 @@ class AonhubSeederController @Inject() (reactiveMongoApi: ReactiveMongoApi)(wsCl
   def mangaRepo = new AonhubMangaRepoImpl(reactiveMongoApi)
   
   def readMangaList(): String = {
+    // AonhubSearchData
     var f = FileUtils.getFile( resourcesRoot + "manga.json")
     FileUtils.readFileToString(f, "UTF-8")
+  }
+  
+  // read the results of writeMangaToString
+  def readMangaById(mangaId: Int): Option[AonhubManga] = {
+    val strPath = resourcesRoot + "json/" // "resources/sources/aonhub/json/"
+    val filePath = strPath + mangaId.toString()
+//    Logger.debug("readMangaById: " + mangaId + " => " + filePath)
+    val fileJson = FileUtils.getFile(filePath)
+    if (fileJson.exists()) {
+//      Logger.debug(filePath + " exist!")
+      val strJson = FileUtils.readFileToString(fileJson, "UTF-8")
+//      Logger.debug(strJson)
+      val jsv = Json.parse(strJson)
+      val manga = jsv.as[AonhubManga]
+      Some(manga)
+    } else {
+      None
+    }
   }
   
   def writeMangaToString(manga: AonhubManga) = {
@@ -59,6 +78,53 @@ class AonhubSeederController @Inject() (reactiveMongoApi: ReactiveMongoApi)(wsCl
     val fileSave = FileUtils.getFile(strPath + manga.id)
     FileUtils.writeStringToFile(fileSave, strContent)
     
+  }
+  
+  def downloadImageToDir(dirName: String, imageName: String, imageUrl: String) = {
+    Logger.debug("downloading image: " + imageUrl)
+    val dir = FileUtils.getFile(dirName)
+    if ( !dir.exists() ) {
+          FileUtils.forceMkdir(dir)
+    }
+    val strFile = dirName + "/" + imageName
+    val fileDest = FileUtils.getFile(strFile)
+    val wsRequest = wsClient.url(imageUrl)
+    val fwsResponse = wsRequest.get()
+    val fImageBytes = fwsResponse.map( wsres => {
+      wsres.bodyAsBytes
+    })
+    fImageBytes.map( bytes => {
+      FileUtils.writeByteArrayToFile(fileDest, bytes.toArray[Byte])
+    })
+//    val bytes = Await.result(fImageBytes, Duration.Inf)
+//    FileUtils.writeByteArrayToFile(fileDest, bytes.toArray[Byte])
+  }
+  
+  def downloadAllPages(manga: AonhubManga): Boolean = {
+    val strMangaRootDir = "resources/sources/aonhub/manga/"
+    
+    var iLastSlash = manga.imageUrl.lastIndexOf("/")
+    val mangaDirName = manga.id.toString()
+    val mangaThumbName = manga.imageUrl.substring(iLastSlash+1)
+    downloadImageToDir(strMangaRootDir + mangaDirName, mangaThumbName, manga.imageUrl)
+    var bSuccess = true;
+    manga.chapters.foreach( ch => {
+      val pagesDir = strMangaRootDir + mangaDirName + "/" + ch.name
+      ch.pages.foreach( pageUrl => {
+        iLastSlash = pageUrl.lastIndexOf("/")
+        val pageName = pageUrl.substring(iLastSlash+1)
+        try {
+          downloadImageToDir(pagesDir, pageName, pageUrl)
+        } catch {
+          case ex: Exception => {
+            bSuccess = false
+            Logger.error("Exception downloading " + pageUrl + " => " + ex.getMessage())
+            Logger.error("Stack trace: " + ex.getStackTrace.toString())
+          }
+        }
+      })
+    })
+    bSuccess
   }
   
   def updatemangajson(): Action[AnyContent] = Action.async {
@@ -143,31 +209,56 @@ class AonhubSeederController @Inject() (reactiveMongoApi: ReactiveMongoApi)(wsCl
     }
   }
   
-  def downloadAllPages(manga: AonhubManga) = {
-    // todo: download each of the pages
-  }
-  
   def image(mangaId: Int): Action[AnyContent] = Action.async {
-    // todo: downloadAllPages(manga)
-    Future {
-      Ok("done")
+    val oManga = readMangaById(mangaId)
+    if ( oManga.isDefined ) {
+      val manga = oManga.get
+      val bResult = downloadAllPages(manga)
+      if ( bResult ) {
+        Future {
+          Ok("downloaded: " + mangaId)
+        }
+      } else {
+        Future {
+          Ok("unable to download: " + mangaId)
+        }
+      }
+    } else {
+      Future {
+        Ok("manga not found: " + mangaId)
+      }
     }
   }
   
+  // never tested: use image(mangaId) instead?
   def imagefromto(startingIndex: Int, endingIndex: Int): Action[AnyContent] = Action.async {
-    val strManga = readMangaList()
-    val jsv = Json.parse(strManga)
-    val lMangaSearchData = jsv.as[List[AonhubSearchData]]
-    val iStartingIndex = if (startingIndex < 0 || startingIndex >= lMangaSearchData.length) 0 else startingIndex
-    var iEndingIndex = if ( endingIndex < 0 || endingIndex >= lMangaSearchData.length ) lMangaSearchData.length-1 else endingIndex
-    if ( iEndingIndex < iStartingIndex ) {
-      iEndingIndex = iStartingIndex+1
-    }
+//    val strManga = readMangaList()
+//    val jsv = Json.parse(strManga)
+//    val lMangaSearchData = jsv.as[List[AonhubSearchData]]
+//    val iStartingIndex = if (startingIndex < 0 || startingIndex >= lMangaSearchData.length) 0 else startingIndex
+//    var iEndingIndex = if ( endingIndex < 0 || endingIndex >= lMangaSearchData.length ) lMangaSearchData.length-1 else endingIndex
+//    if ( iEndingIndex < iStartingIndex ) {
+//      iEndingIndex = iStartingIndex+1
+//    }
+    val iStartingIndex = startingIndex
+    val iEndingIndex = endingIndex
     var iCurrentIndex = iStartingIndex
     var iCount = 0
     for ( iCurrentIndex <- iStartingIndex to iEndingIndex ) {
       try {
         // todo: downloadAllPages(manga)
+        val oManga = readMangaById(iCurrentIndex)
+        if ( oManga.isDefined ) {
+          val manga = oManga.get
+          val bResult = downloadAllPages(manga)
+          if ( bResult ) {
+            Logger.error("Downloaded all pages: " + iCurrentIndex + ". " + manga.name)
+          } else {
+            Logger.error("Failed to download pages: " + iCurrentIndex + ". " + manga.name)
+          }
+        } else {
+          Logger.error("Unable to find: " + iCurrentIndex)
+        }
       } catch {
           case ex: Exception => {
             Logger.error("Exception: " + iCurrentIndex + ex.getMessage())

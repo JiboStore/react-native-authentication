e5a1cbe76b27eb450c486d9b416051cc4d73b2a6
Hakim Hauston <hakim.hauston@gmail.com>
2017-08-15 13:51:31 +0800

    [mangafun] curl commands to download manga info and images

---
+++ aonhub-getmanga.sh
@@ -0,0 +1,7 @@
+curl http://localhost:3005/aonhub/seeder/getmangainfo?mangaId=2940 # I"s
+curl http://localhost:3005/aonhub/seeder/getmangainfo?mangaId=1541 # Doraemon
+curl http://localhost:3005/aonhub/seeder/getmangainfo?mangaId=1404 # Rival
+curl http://localhost:3005/aonhub/seeder/getmangainfo?mangaId=3236 # Love Hina
+curl http://localhost:3005/aonhub/seeder/getmangainfo?mangaId=3218 # GTO
+curl http://localhost:3005/aonhub/seeder/getmangainfo?mangaId=79   # Conan
+curl http://localhost:3005/aonhub/seeder/getmangainfo?mangaId=5    # Fairy Tail
---
+++ aonhub-getpage.sh
@@ -0,0 +1,7 @@
+#curl http://localhost:3005/aonhub/seeder/image?mangaId=2940 # I"s
+curl http://localhost:3005/aonhub/seeder/image?mangaId=1541 # Doraemon
+curl http://localhost:3005/aonhub/seeder/image?mangaId=1404 # Rival
+curl http://localhost:3005/aonhub/seeder/image?mangaId=3236 # Love Hina
+curl http://localhost:3005/aonhub/seeder/image?mangaId=3218 # GTO
+curl http://localhost:3005/aonhub/seeder/image?mangaId=79   # Conan
+curl http://localhost:3005/aonhub/seeder/image?mangaId=5    # Fairy Tail

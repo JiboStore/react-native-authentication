a740a6c21f76f8ce6714e5de13aaf39aee3151d3
Hakim Hauston <hakim.hauston@gmail.com>
2017-10-11 18:44:45 +0800

    [mangafun] client: android version first build!

--- client/MangafunApp/index.android.js
+++ client/MangafunApp/index.android.js
@@ -12,42 +12,31 @@ import {
   View
 } from 'react-native';
 
+import {
+  Provider,
+  connect
+} from 'react-redux';
+
+import store from './src/store/store';
+
+import LogOrSign from './src/scenes/logorsign/logorsign'
+// import SignUpSeq from './src/scenes/signupseq/index'
+import SignUpSeq from './src/scenes/signupseq/index'
+
 export default class MangafunApp extends Component {
   render() {
+    let RootView = SignUpSeq;
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
+      <Provider store={store}>
+        <View style={{flex:1}}>
+          <RootView/>
+        </View>
+      </Provider>
+    )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
+// AppRegistry.registerComponent('MangafunApp', () => MangafunApp);
+// AppRegistry.registerComponent('MangafunApp', () => LogOrSign);
+// AppRegistry.registerComponent('MangafunApp', () => SignUpSeq);
 AppRegistry.registerComponent('MangafunApp', () => MangafunApp);

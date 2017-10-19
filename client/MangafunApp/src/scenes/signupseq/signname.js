df48a16dabbf51ea7089c11938aaa368e863b52d
Hakim Hauston <hakim.hauston@gmail.com>
2017-10-19 15:29:40 +0800

    [mangafun] client: react-native-device-info works in iOS and android
    Android: device-id is the same across re-install
    iOS: device-id is the same across re-install too

--- client/MangafunApp/src/scenes/signupseq/signname.js
+++ client/MangafunApp/src/scenes/signupseq/signname.js
@@ -16,6 +16,9 @@ import {
   View
 } from 'react-native';
 
+import DeviceInfo from 'react-native-device-info';
+// var DeviceInfo = require('react-native-device-info');
+
 import { StackNavigator } from 'react-navigation';
 
 import {
@@ -81,6 +84,17 @@ class SignNameScreen extends Component {
     Alert.alert("hello: " + payload.email)
   }
   componentDidMount = () => {
+    // test react-native-device-info
+    console.log("Device Unique ID", DeviceInfo.getUniqueID());  // e.g. FCDBD8EF-62FC-4ECB-B2F5-92C9E79AC7F9
+    console.log("Device Manufacturer", DeviceInfo.getManufacturer());  // e.g. Apple
+    console.log("Device Model", DeviceInfo.getModel());  // e.g. iPhone
+    console.log("Device Name", DeviceInfo.getSystemName());  // e.g. iPhone OS
+    console.log("Device Version", DeviceInfo.getSystemVersion());  // e.g. 9.0
+    console.log("Bundle Id", DeviceInfo.getBundleId());  // e.g. com.learnium.mobile
+    console.log("Build Number", DeviceInfo.getBuildNumber());  // e.g. 89
+    console.log("App Version", DeviceInfo.getVersion());  // e.g. 1.1.0
+    console.log("App Version (Readable)", DeviceInfo.getReadableVersion());  // e.g. 1.1.0.89
+    
     AsyncStorage.getItem("name").then((szObj) => {
       if ( szObj != null ) {
         const obj = JSON.parse(szObj);
